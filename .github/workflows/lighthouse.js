d71052c6dc694f817a55e95119b265c75422cfcc
RedGuy12 <paul@reid-family.org>
2021-08-25 16:36:53 -0500

    Updates from https://github.com/onedotprojects/auth/tree/develop/.github/workflows/

--- .github/workflows/lighthouse.js
+++ .github/workflows/lighthouse.js
@@ -12,7 +12,6 @@ import fetch from "node-fetch";
  */
 function graphql(query, ...placeholderValues) {
 	return (
-		// eslint-disable-next-line unicorn/no-array-reduce -- reduce is the best way to do this.
 		placeholderValues.reduce(
 			(last, placeholder, index) => `${last}${placeholder}${query[index + 1]}`,
 			query[0],
@@ -25,21 +24,21 @@ function graphql(query, ...placeholderValues) {
  *
  * @param {string} body - Body of the comment.
  *
- * @returns {Promise<any>} - Result from GitHub's GraphQL API.
+ * @returns {Promise<unknown>} - Result from GitHub's GraphQL API.
  */
 function commentOnDiscussion(body) {
 	return fetch("https://api.github.com/graphql", {
 		body: JSON.stringify({
-			// Hmmst… query strings break prettier formatting...mustache? maybe
+			// TODO: use gh action
 			query: graphql`mutation {
 					addDiscussionComment(
-						input: {discussionId: "MDEwOkRpc2N1c3Npb24zNDY2NDAy", body: "${body}"}
-					) {
-					  comment {
-						id
-					  }
-					}
-				  }`,
+						input: {discussionId: "MDEwOkRpc2N1c3Npb24zNDY2NDAy", body: ${JSON.stringify(body)}}
+						) {
+						  comment {
+							id
+						  }
+						}
+					  }`,
 		}),
 
 		headers: {
@@ -48,28 +47,59 @@ function commentOnDiscussion(body) {
 		},
 
 		method: "POST",
-	}).then((response) => response.json());
+	}).then((result) => result.text());
 }
 
 /**
  * Transpose array.
  *
- * @param {any[][]} matrix
+ * @template T
+ * @param {T[][]} array - Array to transpose.
+ *
+ * @returns {T[][]} - Transposed array.
  */
-function transpose(matrix) {
-	return matrix[0].map((_, i) => matrix.map((row) => row[i]));
+ function transpose(array) {
+	return array[0].map((_, index) => array.map((row) => row[+index]));
+}
+
+/**
+ * Get the average of an array.
+ *
+ * @param {number[]} array - Array to average.
+ *
+ * @returns {number} - The average.
+ */
+function getAverage(array) {
+	return array.reduce((accumulated, next) => accumulated + next, 0) / array.length;
 }
 
 /**
  * Add emoji colored based on a number.
  *
- * @param {number} number
+ * @param {number} number - Number to base the emoji off of.
+ *
+ * @returns {string} - An appropriately colored emoji followed by the number.
  */
 function addEmoji(number) {
 	return `${number < 50 ? "🔴" : number < 90 ? "🟡" : "🟢"} ${number}`;
 }
 
-/** @type {import("../../types").lighthouseResult} */
+/**
+ * @type {{
+ * 	code: string;
+ * 	data: {
+ * 		url: string;
+ * 		emulatedFormFactor: string;
+ * 		scores: {
+ * 			accessibility: number;
+ * 			bestPractices: number;
+ * 			performance: number;
+ * 			progressiveWebApp: number;
+ * 			seo: number;
+ * 		};
+ * 	}[];
+ * }}
+ */
 let data;
 
 try {
@@ -80,54 +110,52 @@ try {
 	if (data.code !== "SUCCESS") throw new Error(`code: ${data.code}`);
 } catch (error) {
 	commentOnDiscussion(
-		`An error occured while retrieving the data from Lighthouse.\n\`\`\`js\n${error}\n\`\`\``,
+		`An error occurred while retrieving the data from Lighthouse.\n\`\`\`js\n${error}\n\`\`\``,
 	);
 
 	throw new Error(error);
 }
 
 try {
-	const allScores = transpose(data.data.map(({ scores: s }) => Object.values(s))).map(
-			(s) => s.reduce((a, b) => a + b, 0) / s.length,
-		),
-		output =
-			"# Today’s Lighthouse scores\n\n" +
-			"<table><thead><th>URL<td>Device</td>" +
-			"<td>Accessibility</td>" +
-			"<td>Best Practices</td>" +
-			"<td>Performace</td>" +
-			"<td>Progressive Web App</td>" +
-			"<td>SEO</td>" +
-			"<td>Overall</td>" +
-			"<td>PageSpeed Insights</td></th></thead><tbody>" +
-			data.data.reduce((accumulated, result) => {
-				const scores = Object.values(result.scores);
-				return (
-					accumulated +
-					`<tr><td>${
-						(result.url[result.url.length - 1] === "/" ? result.url : result.url + "/")
-							.trim()
-							.split(/^(?:https?:\/\/)?.+\..+?(?=\/)/iu)[1]
-					}</td>` +
+	const allScores = transpose(data.data.map(({scores}) => Object.values(scores)))
+		.map(getAverage).splice(2, 1);
+
+	commentOnDiscussion(
+		`${
+			"<h2>Today’s Lighthouse scores</h2><br /> <br />" +
+			"<table><thead><tr><th>URL</th>" +
+			"<th>Device</th>" +
+			"<th>Accessibility</th>" +
+			"<th>Best Practices</th>" +
+			"<th>Performace</th>" +
+			"<th>SEO</th>" +
+			"<th>Overall</th>" +
+			"<th>PageSpeed Insights</th></tr></thead><tbody>"
+		}${data.data.reduce((accumulated, result) => {
+			const scores = Object.values(result.scores).splice(2, 1);
+
+			return (
+				`${accumulated}<tr><td><a href="${result.url.trim()}">${
+					(result.url[result.url.length - 1] === "/" ? result.url : `${result.url}/`)
+						.trim()
+						.split("https://homeschool.rebeccareid.com")[1]
+					}</a></td>` +
 					`<td>${result.emulatedFormFactor}</td>` +
 					`<td>${scores.map(addEmoji).join("</td><td>")}</td>` +
-					`<td>${addEmoji(scores.reduce((a, b) => a + b, 0) / scores.length)}</td>` +
-					`<td><a href="//developers.google.com/speed/pagespeed/insights/` +
-					`?url=${encodeURIComponent(result.url.trim())}&tab=${
-						result.emulatedFormFactor
-					}">More information</a></td></tr>`
-				);
-			}, {}) +
-			`</tbody><tfoot><tr><td colspan="2"><b>Overall</b></td>` +
+					`<td>${addEmoji(getAverage(scores))}</td><td>` +
+					`<a href="//developers.google.com/speed/pagespeed/insights/?url=${encodeURIComponent(
+						`${result.url.trim()}`,
+					)}&tab=${result.emulatedFormFactor}">More information</a></td></tr>`
+			);
+		}, "")}</tbody><tfoot><tr><td colspan="2"><b>Overall</b></td>` +
 			`<td><b>${allScores.map(addEmoji).join("</b></td><td><b>")}</b></td>` +
 			`<td colspan="2"><b><i>${addEmoji(
-				allScores.reduce((a, b) => a + b, 0) / allScores.length,
-			)}</i></b></td></tr></tbody></table>`;
-
-	commentOnDiscussion(output);
+				getAverage(allScores),
+			)}</i></b></td></tr></tbody></table>`,
+	);
 } catch (error) {
 	commentOnDiscussion(
-		"An error occured while generating the comment.\n" +
+		"An error occurred while generating the comment.\n" +
 			`\`\`\`js\n${JSON.stringify(error)}\n\`\`\``,
 	);
 
